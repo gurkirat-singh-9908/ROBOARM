4e073127a3e6380996e40e81ead8399001ccd02a
gurkirat-singh-9908 <gurkiratsinghvirdi10@gmail.com>
2026-03-01 18:31:43 +0530

    urdf corrected

--- ManualPhase1/Ros/bkup/roboticarm_description/build/roboticarm_description/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kira/roboticarm_description/install/roboticarm_description'
---
+++ ManualPhase1/Ros/build/roboticarm_description/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/kirat/Desktop/ROBOARM/ManualPhase1/Ros/install/roboticarm_description'
