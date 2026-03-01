7ee7e71d14f92d2dd2ac962ddd6b13c25386cde5
gurkirat-singh-9908 <gurkiratsinghvirdi10@gmail.com>
2026-03-01 23:21:49 +0530

    Move It ready

---
+++ ManualPhase1/Moveit/build/roboticarm_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kirat/Desktop/ROBOARM/ManualPhase1/Moveit/src/roboticarm_moveit_config
+# Build directory: /home/kirat/Desktop/ROBOARM/ManualPhase1/Moveit/build/roboticarm_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ManualPhase1/Ros/build/roboticarm_moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/kirat/Desktop/ROBOARM/ManualPhase1/Ros/install/roboticarm_description/share/roboticarm_description/package.sh"
