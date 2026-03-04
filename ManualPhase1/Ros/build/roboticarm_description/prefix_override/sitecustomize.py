7eec6d2a69c22b0cce4842c04cc5b8cc7e46f519
gurkirat-singh-9908 <gurkiratsinghvirdi10@gmail.com>
2026-03-04 12:57:50 +0530

    Local changes before sync

--- ManualPhase1/Ros/build/roboticarm_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kirat/Desktop/ROBOARM/ManualPhase1/Ros/install/roboticarm_description'
