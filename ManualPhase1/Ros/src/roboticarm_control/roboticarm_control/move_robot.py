5269ccc8d463dc12069bef706aadbc062d8f964a
gurkirat-singh-9908 <gurkiratsinghvirdi10@gmail.com>
2026-03-07 01:57:12 +0530

    robotic arm control via positions algo(not finshed)

---
+++ ManualPhase1/Ros/src/roboticarm_control/roboticarm_control/move_robot.py
@@ -0,0 +1,70 @@
+import rclpy
+from rclpy.node import Node
+from moveit_msgs.action import MoveGroup
+from moveit_msgs.msg import Constraints, JointConstraint, MotionPlanRequest
+from rclpy.action import ActionClient
+
+
+class MoveRobot(Node):
+
+    def __init__(self):
+        super().__init__('move_robot')
+
+        self.client = ActionClient(self, MoveGroup, 'move_action')
+
+        self.get_logger().info("Waiting for MoveIt action server...")
+        self.client.wait_for_server()
+
+        self.send_goal()
+
+
+    def send_goal(self):
+
+        goal_msg = MoveGroup.Goal()
+
+        request = MotionPlanRequest()
+        request.group_name = "arm"   # IMPORTANT
+
+        constraint = Constraints()
+
+        joint_names = [
+            "Revolute_22",
+            "Revolute_23",
+            "Revolute_24",
+            "Revolute_25",
+            "Revolute_26",
+            "Revolute_27"
+        ]
+
+        target_positions = [0.0, -0.4, 0.3, 0.0, 0.0, 0.0]
+
+        for name, pos in zip(joint_names, target_positions):
+
+            jc = JointConstraint()
+            jc.joint_name = name
+            jc.position = pos
+            jc.tolerance_above = 0.01
+            jc.tolerance_below = 0.01
+            jc.weight = 1.0
+
+            constraint.joint_constraints.append(jc)
+
+        request.goal_constraints.append(constraint)
+
+        goal_msg.request = request
+
+        self.get_logger().info("Sending motion request")
+
+        self.client.send_goal_async(goal_msg)
+
+
+def main():
+    rclpy.init()
+    node = MoveRobot()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
