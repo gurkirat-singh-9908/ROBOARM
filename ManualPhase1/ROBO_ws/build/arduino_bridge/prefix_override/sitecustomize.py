4c0c75fe60e4a4b0cda2370b55ad3882b036a236
gurkirat-singh-9908 <gurkiratsinghvirdi10@gmail.com>
2026-03-07 15:46:31 +0530

    changes

--- ManualPhase1/ROBO_ws/build/arduino_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kv3/ROBOARM/ManualPhase1/ROBO_ws/install/arduino_bridge'
--- ManualPhase1/ROBO_ws/build/roboticarm_moveit2/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/kv3/ROBOARM/ManualPhase1/ROBO_ws/install/roboticarm_description/share/roboticarm_description/package.sh"
